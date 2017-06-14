60bab8b62974a508bc6cd3855fadd3e3e6e4e0be
grullo90 <g.rullo@expandjs.com>
2017-06-14 15:50:57 +0200

    Release 1.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 // Const
-const webpack = require('webpack');
+const Uglify = require('uglifyjs-webpack-plugin');
 
 // Exporting
 module.exports = {
@@ -12,9 +12,9 @@ module.exports = {
     },
     output: {
         filename: 'xp-primus.js',
-        path: './dist'
+        path: `${__dirname}/dist`
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}, output: {comments: false}})
+        new Uglify({compress: {warnings: false}, output: {comments: false}})
     ]
 };
